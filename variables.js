51b9fcdddc01db03330caed794df18fc1e46ec32
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-05-10 21:57:52 -0500

    Variables

---
+++ variables.js
@@ -0,0 +1,20 @@
+var age; // Declare
+age = 14; // initialize
+
+var me = {
+  name: "Eliaz",
+  age: 14,
+};
+
+var username = "UltiRequiem"; // Declare and initialize in the same line
+
+var hobbies = ["Code", "Music", "Anime", "Manga", "Maths"]; // Arrays
+
+// Print in console
+console.log("Hi! My name is " + username + " and I'm " + age + " years old.");
+
+console.log("Some of my hobbies are:");
+// For
+for (let i = 0; i < hobbies.length; i++) {
+  console.log("- " + hobbies[i]);
+}
