113b903cca58ad6aedd258ec103f8b9727f9f6e7
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-05-12 12:10:16 -0500

    Update

--- loops.js
+++ loops.js
@@ -10,12 +10,12 @@ for (var i = 0; i < students.length; i++) {
 }
 
 console.log("Using new form:")
-for (var estudiante of students) {
-    helloStudent(estudiante);
+for (var student of students) {
+    helloStudent(student);
 }
 
 console.log("Using While loop:")
 while (students.length > 0) { // Aquí la tarea se hará siempre y cuando sea true, cuando llegué a false, dejará de hacer la tarea
-    var estudiante = students.shift(); // shift() es un método que saca un elemento del array de la posición 0 a la última, Pop() comienza de la última a la primera.
-    helloStudent(estudiante);
+    var student = students.shift(); // shift() es un método que saca un elemento del array de la posición 0 a la última, Pop() comienza de la última a la primera.
+    helloStudent(student);
 }
